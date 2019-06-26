60defcd22852cae2bcc25bc7d76a08b76f3945f6
Che5ter <hatake18@bk.ru>
2019-06-26 23:43:28 +0300

    4 задание, 2 из 3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var OBJNUM = 8; // количество объектов, которое нужно создать
+var PIN_NUMBER = 8; // количество объектов, которое нужно создать
 var appartments = ['palace', 'flat', 'house', 'bungalo']; // массив с видами жилья
 var MapWidth = document.querySelector('.map').offsetWidth; // находим ширину блока .map
-var map = document.querySelector('.map');
-
-map.classList.remove('map--faded');
+var templatePin = document.querySelector('#pin').content.querySelector('button');
+var mapPin = document.querySelector('.map__pins');
+var fragment = document.createDocumentFragment();
 
 var makeOffer = function (massive, min, max) { // создает рандомный номер массива с видами жилья
   return massive[Math.floor(Math.random() * (max - min)) + min];
@@ -19,46 +19,53 @@ var makeRandomNum = function (min, max) { // создает рандомное �
 
 var objArray = []; // массив куда будем записывать сгенерированные объекты
 
-var generateObjectives = function (quantity) {
+var pins = {
+  generateObjectives: function () {
 
-  for (var i = 0; i < quantity; i++) { // генерируем объекты и пушим их в массив objArray
-    var somePin = {
-      'author': {
-        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-      },
-      'offer': {
-        'type': makeOffer(appartments, 0, appartments.length - 1)
-      },
+    for (var i = 0; i < PIN_NUMBER; i++) { // генерируем объекты и пушим их в массив objArray
+      var somePin = {
+        'author': {
+          'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+        },
+        'offer': {
+          'type': makeOffer(appartments, 0, appartments.length - 1)
+        },
 
-      'location': {
-        'x': makeRandomNum(0, MapWidth),
-        'y': makeRandomNum(130, 630)
-      }
-    };
-    objArray.push(somePin);
+        'location': {
+          'x': makeRandomNum(0, MapWidth),
+          'y': makeRandomNum(130, 630)
+        }
+      };
+      objArray.push(somePin);
+    }
+  },
+  createElements: function () {
+    for (var i = 0; i < objArray.length; i++) {
+      var element = templatePin.cloneNode(true);
+      element.style.left = objArray[i].location.x - 25 + 'px';
+      element.style.top = objArray[i].location.y - 70 + 'px';
+      element.querySelector('img').src = objArray[i].author.avatar;
+      element.querySelector('img').alt = 'заголовок объявления';
+      fragment.appendChild(element);
+    }
+  },
+  addFragment: function (element) {
+    element.appendChild(fragment);
   }
 };
 
-
-var templatePin = document.querySelector('#pin').content.querySelector('button');
-var mapPin = document.querySelector('.map__pins');
-var fragment = document.createDocumentFragment();
-
-var createElements = function () {
-  for (var i = 0; i < objArray.length; i++) {
-    var element = templatePin.cloneNode(true);
-    element.style.left = objArray[i].location.x - 25 + 'px';
-    element.style.top = objArray[i].location.y - 70 + 'px';
-    element.querySelector('img').src = objArray[i].author.avatar;
-    element.querySelector('img').alt = 'заголовок объявления';
-    fragment.appendChild(element);
+var mainPin = document.querySelector('.map__pin--main');
+mainPin.addEventListener('click', function () {
+  var map = document.querySelector('.map');
+  map.classList.remove('map--faded');
+  var form = document.querySelector('.ad-form');
+  form.classList.remove('ad-form--disabled');
+  var fieldsets = document.querySelectorAll('fieldset');
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].disabled = false;
   }
-};
-
-var addFragment = function (element) {
-  element.appendChild(fragment);
-};
-
-generateObjectives(OBJNUM);
-createElements();
-addFragment(mapPin);
+  var headerUpload = document.querySelector('.ad-form-header__input');
+  headerUpload.disabled = false;
+  var filters = document.querySelector('map__filters');
+  filters.classList.
+});
